0ee83924f856fc4a810aced9d30879823d795785
Matteo V <matteo.vayr@gmail.com>
2023-02-06 10:33:52 +0100

    repository_remoto_da_linea_di_comando

---
+++ form_in_php/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="log-in-response.php" method="post">
+Name: <input type="text" name="name"><br>
+Cognome: <input type="text" name="last_name"><br>
+E-mail: <input type="email" name="email"><br>
+Password <input type="password" name="password" value="segretissimo"><br>
+
+
+<input type="submit">
+</form>
+<!-- GET e POST -->
+<!-- il metodo get lascia gli argomenti nell'indirizzo di ricerca, sarebbe quindi possibile ricostruire l'intera pagina dal suo indirizzo -->
+<!-- col metodo post nell'indirizzo i dati non sono visibili -->
+
+
+</body>
---
+++ form_in_php/log-in-response.php
@@ -0,0 +1,11 @@
+<h1>Sono la risposta (RESPONSE)</h1>
+
+
+<?php
+// pre serve a impaginare l'array
+echo "<pre>";
+print_r($_GET);
+echo "</pre>"
+
+
+?>
