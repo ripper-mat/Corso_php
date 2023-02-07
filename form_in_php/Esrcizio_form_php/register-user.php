7a7c0a3c6cee4e116b8ee00a071fd4c569cf564d
Matteo V <matteo.vayr@gmail.com>
2023-02-07 10:58:50 +0100

    fine response 2

--- form_in_php/Esrcizio_form_php/register-user.php
+++ form_in_php/Esrcizio_form_php/register-user.php
@@ -1,9 +1,13 @@
 <?php
+filter_input(INPUT_GET, "first_name");       
+filter_input(INPUT_GET, "last_name");       
+filter_input(INPUT_GET, "birthday");       
+filter_input(INPUT_GET, "birth_place");       
+filter_input(INPUT_GET, "gender");       
+filter_input(INPUT_GET, "email");       
+filter_input(INPUT_GET, "password");       
 
-        echo "<h2>Congratulazioni ".$_POST["first_name"]." la registrazione è andata a buon fine</h2>";
-        echo "<pre>";
-        echo "post:";
-        print_r($_POST);
-        echo "</pre>";         
-
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
 ?>
