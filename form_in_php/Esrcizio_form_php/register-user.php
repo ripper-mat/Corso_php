e06574a1d43eb1beab433eb64cfa736eb00ff913
Matteo V <matteo.vayr@gmail.com>
2023-02-07 11:51:11 +0100

    filter input

--- form_in_php/Esrcizio_form_php/register-user.php
+++ form_in_php/Esrcizio_form_php/register-user.php
@@ -1,11 +1,17 @@
 <?php
-filter_input(INPUT_GET, "first_name");       
-filter_input(INPUT_GET, "last_name");       
-filter_input(INPUT_GET, "birthday");       
-filter_input(INPUT_GET, "birth_place");       
-filter_input(INPUT_GET, "gender");       
-filter_input(INPUT_GET, "email");       
-filter_input(INPUT_GET, "password");       
+filter_input(INPUT_POST, "first_name");       
+filter_input(INPUT_POST, "last_name");       
+filter_input(INPUT_POST, "birthday");       
+filter_input(INPUT_POST, "birth_place");       
+filter_input(INPUT_POST, "gender");       
+// filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);       
+filter_input(INPUT_POST, "password");  
+
+if (!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL)) {
+        echo("Email is not valid");
+    } else {
+        echo("Email is valid");
+    }
 
 echo "<pre>";
 print_r($_POST);
